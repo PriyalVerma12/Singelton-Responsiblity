2364014ee2ed89909c4024635ab29211f8bbffad
PriyalVerma <154815764+PriyalVerma12@users.noreply.github.com>
2025-06-16 00:26:00 +0530

    LiskovSubstitution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,62 +1,74 @@
 package org.example;
-/// Single Responsibility Principle (SRP) Example
-// class should have only one reason to change which means every class should have single responsibility or single jb or single purpose
-//  //class with multiple responsiblity
-//class Baker{
-//    public void bakebBread(){
-//        System.out.println("Baking braed");
-//    }
-//     public void manageInventory(){
-//         System.out.println("manages Inventory");
-//     }
-//      public void manageSuppliers() {
-//          System.out.println("manages suppliers");
-//      }
-//      public void manageCustomer(){
-//          System.out.println("Hi customer");
-//      }
-//}
-
-//
-//
-//    public class Main{
-//        public static void main(String[] args) {
-//            Baker baker = new Baker();
-//            baker.bakebBread();
-//            baker.manageInventory();
-//            baker.manageSuppliers();
-//            baker.manageCustomer();
-//
-//
-//
+////Liskov Substitution Principle
+////According to this principle Derived child classes must be substitutable for their base classes without affecting the correctness of the program.
+////Problemmatic apparoach
+//class Vehicle{
+//     public void startEngine(){
+//        System.out.println("Engine is starting");
 //    }
 //}
+//class Car extends Vehicle{
+//    @Override
+//    public void startEngine(){
+//        System.out.println(" mmmmmm");
+//    }
+//}
+//class Bicycle extends Vehicle {
+//    @Override
+//    public void startEngine() {
+//        // bicycle has no engine
+//        throw new UnsupportedOperationException("Bicycle doesn't have engine");
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        Vehicle vehicle = new Car();
+//        vehicle.startEngine(); //
+//
+//
+//        vehicle = new Bicycle(); // This will cause an issue
+//        vehicle.startEngine(); // Throws UnsupportedOperationException
+//    }
+//}
 
-// class with single responsibility
-class Baker{
-    public void bakingBread(){
-        System.out.println("Baking bread");
+// correct way
+abstract class Vehicle{
+    // common vehicle behaviours
+    public void move(){
+        System.out.println("Vehicle is moving");
+
     }
 }
-class InventoryManager{
-    public void manageInventory(){
-        System.out.println("Manage Inventory");
+abstract class EngineVehicle extends Vehicle {
+    public void startEngine(){
+        System.out.println("Engine is starting");
     }
 }
-class HandlingCustomers {
-    public void manageCustomers() {
-        System.out.println("Hi customers .. welcome to our bakery");
-
+abstract class NonEngine extends Vehicle{
+    public void working(){
+        System.out.println("Good for environment ");
+    }
+}
+class Car extends EngineVehicle{
+    @Override
+    public void startEngine(){
+        System.out.println("Lets go for a ride");
+    }
+}
+class Bicycle extends NonEngine{
+    @Override
+    public void working() {
+        System.out.println("Bicycle is eco-friendly and good for health");
     }
 
     public static void main(String[] args) {
-        Baker baker = new Baker();
-        baker.bakingBread();
+        //using EnineVehicle
 
-        InventoryManager inventoryManager = new InventoryManager();
-        inventoryManager.manageInventory();
-
-        HandlingCustomers handlingCustomers = new HandlingCustomers();
-        handlingCustomers.manageCustomers();
+        EngineVehicle car = new Car();
+        car.startEngine();
+        car.move();
+       NonEngine bicycle = new Bicycle();
+        bicycle.working();
+        bicycle.move(); // Bicycle can still move, but it doesn't have an engine
     }
 }
